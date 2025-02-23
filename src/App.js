2676ad3742091a040ae14256e5274b5e3557dc75
Julio Vazquez <juliovazquezm01@gmail.com>
2025-02-22 18:46:30 -0600

    completando y eliminando TODOS

--- src/App.js
+++ src/App.js
@@ -31,7 +31,23 @@ function App() {
     }
   );
 
-  console.log('Los Usuarios Buscaron todos de ' + searchValue);
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    );
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    );
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  };
 
   return (
     <>
@@ -47,9 +63,12 @@ function App() {
           searchedTodos.map(
             todo => (
               <TodoItem 
-              key={todo.text} 
-              text={todo.text}
-              completed={todo.completed} />
+                key={todo.text} 
+                text={todo.text}
+                completed={todo.completed}
+                onComplete={() => completeTodo(todo.text)}
+                onDelete={() => deleteTodo(todo.text)}
+              />
             ))
         }
       </TodoList>
